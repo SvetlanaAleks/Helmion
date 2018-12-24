8f1e0ee297007f1983b0ecb186b134c996e46f13
Yulian-Caesar <yulik19971997@gmail.com>
2018-12-24 18:32:54 +0200

    yulik add  parasites block

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,4 +1,47 @@
 //Some JS
+var slider1 = $('.parasites-slider');
 
+function initSlider(slider, options) {
+    slider.on('init', function () {
+        setTimeout(function () {
+            slider.addClass('is-ready');
+        }, 100);
+    });
+    slider.not('.slick-initialized').slick(options);
+}
 
+function destroySlider(slider) {
+    if (slider.hasClass('slick-initialized')) {
+        slider.slick('unslick');
+    }
+}
 
+function showSlider() {
+    var tablet = ($(window).width()) < 1024;
+    if (tablet) {
+        initSlider(slider1, {
+            responsive: [{
+                breakpoint: 1023,
+                settings: {
+                    slidesToShow: 2,
+
+                    slidesToScroll: 1
+                }
+            },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+
+                        slidesToScroll: 1
+                    }
+                }
+            ]
+        });
+    } else {
+        destroySlider(slider1);
+    }
+};
+showSlider();
+
+$(window).on('resize', showSlider);
