d421f4967bdba52eb5d0b0a0f7e35725079c6548
Yulian-Caesar <yulik19971997@gmail.com>
2018-12-25 16:17:10 +0200

    yarik help

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -1,6 +1,6 @@
 
 //Some JS
-var slider1 = $('.parasites-slider');
+let slider1 = $('.parasites-slider');
 
 function initSlider(slider, options) {
     slider.on('init', function () {
@@ -10,6 +10,7 @@ function initSlider(slider, options) {
     });
     slider.not('.slick-initialized').slick(options);
 }
+
 
 function destroySlider(slider) {
     if (slider.hasClass('slick-initialized')) {
@@ -21,8 +22,9 @@ function showSlider() {
     var tablet = ($(window).width()) < 1024;
     if (tablet) {
         initSlider(slider1, {
+            variableWidth: true,
             responsive: [{
-                breakpoint: 1023,
+                breakpoint: 1024,
                 settings: {
                     slidesToShow: 2,
 
@@ -42,9 +44,58 @@ function showSlider() {
     } else {
         destroySlider(slider1);
     }
-};
+}
+
 showSlider();
 $(window).on('resize', showSlider);
+
+
+// let slider1 = $('.parasites-slider');
+//
+//
+// function initSlider(slider, options) {
+//     slider.on('init', function () {
+//         setTimeout(function () {
+//             slider.addClass('is-ready');
+//         }, 100);
+//     });
+//     slider.not('.slick-initialized').slick(options);
+// }
+//
+// function destroySlider(slider) {
+//     if (slider.hasClass('slick-initialized')) {
+//         slider.slick('unslick');
+//     }
+// }
+//
+// function showSlider() {
+//     var tablet = ($(window).width()) < 1024;
+//     if (tablet) {
+//         initSlider(slider1, {
+//             responsive: [{
+//                 breakpoint: 1024,
+//                 settings: {
+//                     slidesToShow: 2,
+//
+//                     slidesToScroll: 1
+//                 }
+//             },
+//                 {
+//                     breakpoint: 768,
+//                     settings: {
+//                         slidesToShow: 1,
+//
+//                         slidesToScroll: 1
+//                     }
+//                 }
+//             ]
+//         });
+//     } else {
+//         destroySlider(slider1);
+//     }
+// };
+// showSlider();
+// $(window).on('resize', showSlider);
 
 
 const App = (function () {
