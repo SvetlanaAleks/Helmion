4678f3b612d2631a8b412eef08792df0111740e7
Svetlana Alekseenko <s.alekseenko@everad.com>
2019-01-09 17:25:33 +0200

    finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,11 @@
 //=require ../../node_modules/object-fit-images/dist/ofi.min.js
 
 //=require partials/app.js
+$(document).ready(function () {
+    App.init();
+    new WOW().init();
+});
 
 $('.comments__main').mCustomScrollbar({
     axis: 'y'
-});
-
-$(document).ready(function () {
-    App.init();
 });
